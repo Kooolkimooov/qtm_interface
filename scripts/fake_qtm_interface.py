f03c99809ad21fe9641c7a88db15a06d72cff476
Martin Filliung <martin-filliung@etud.univ-tln.fr>
2025-05-15 08:36:42 +0200

    toward initial position when using fake interface

--- scripts/fake_qtm_interface.py
+++ scripts/fake_qtm_interface.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 from argparse import ArgumentParser, Namespace, RawDescriptionHelpFormatter
-
-from numpy import zeros, eye
-
 from copy import deepcopy
+
+from numpy import array, eye, zeros
+
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from pympc.models.model import Model
-from pympc.models.dynamics.dynamics import Dynamics
 from pympc.models.dynamics.bluerov import Bluerov
+from pympc.models.dynamics.dynamics import Dynamics
 from pympc.models.dynamics.turtlebot import Turtlebot
-from pympc.models.ros_interface.interface import Interface
+from pympc.models.model import Model
 from pympc.models.ros_interface.bluerov import BluerovROSInterface
+from pympc.models.ros_interface.interface import Interface
 from pympc.models.ros_interface.turtlebot import TurtlebotROSInterface
 
 ROBOT_TYPE = [ 'bluerov', 'turtlebot' ]
@@ -64,6 +64,17 @@ def parse_arguments() -> Namespace:
       )
 
   parser.add_argument(
+      "--robot-initial-position",
+      "-i",
+      required = False,
+      type = float,
+      nargs = 6,
+      action = "append",
+      default = [ ],
+      help = "default origin; the initial position of the robot to be simulated,"
+      )
+
+  parser.add_argument(
       "--water-surface-depth",
       "-w",
       required = False,
@@ -75,9 +86,10 @@ def parse_arguments() -> Namespace:
 
   return parser.parse_known_args()[ 0 ]
 
-class Simulation(Model):
-  def __init__(self, robot_type, robot_name, robot_command_topic, water_surface_depth):
-    
+
+class Simulation( Model ):
+  def __init__( self, robot_type, robot_name, robot_command_topic, water_surface_depth, initial_position: list ):
+
     self.robot_type = robot_type
     self.robot_name = robot_name
     self.robot_command_topic = robot_command_topic
@@ -93,35 +105,53 @@ class Simulation(Model):
       self.interface = TurtlebotROSInterface()
     else:
       raise ValueError
-    
-    self.model = Model(dynamics, 0.01, deepcopy(self.interface.initial_state), zeros(dynamics.actuation_size))
+
+    initial_state = zeros( (dynamics.state_size,) )
+    initial_state[ :dynamics.state_size // 2 ] = self.interface.pose_from_ros_pose(
+        self.interface.ros_pose_from_state( array( initial_position + [ 0.0 ] * 6 ) )
+        )
+
+    self.model = Model( dynamics, 0.01, deepcopy( initial_state ), zeros( dynamics.actuation_size ) )
 
     self.pose = PoseWithCovarianceStamped()
     self.pose.header.frame_id = "map"
     self.publisher = rospy.Publisher( f'{self.robot_name}', PoseWithCovarianceStamped, queue_size = 10 )
     self.listener = rospy.Subscriber( f'/{self.robot_command_topic}', self.interface.command_type, self.callback )
 
-    rospy.loginfo( f"simulating {robot_type} on topic {self.publisher.resolved_name} with input from {self.listener.resolved_name}" )
+    rospy.loginfo(
+        f"simulating {robot_type} on topic {self.publisher.resolved_name} with input from {self.listener.resolved_name}"
+        )
 
-  def callback(self, payload ):
+  def callback( self, payload ):
     self.model.actuation = self.interface.actuation_from_ros_actuation( payload )
+
 
 def publish_fake_packets( args: Namespace ):
 
+  if len( args.robot_initial_position ) == 0:
+    args.robot_initial_position = [ [ 0., 0., 0., 0., 0., 0. ] for _ in range( len( args.robot_type ) ) ]
+
   assert (len( args.robot_type ) == len( args.robot_name ) and len( args.robot_type ) == len(
       args.robot_command_topic
-      )), (f"inconsistent number of arguments: each simulated robot must be defined with a -rt, -rn and -rc argument:\n"
-           f"--robot-type:          {args.robot_type}\n"
-           f"--robot-name:          {args.robot_name}\n"
-           f"--robot-command-topic: {args.robot_command_topic} \n")
+      ) and len( args.robot_type ) == len( args.robot_initial_position )), (
+      f"inconsistent number of arguments: each simulated robot must be defined with a -t, -n and -c argument. "
+      f"and if an initial position is given, it must be given for all robots\n"
+      f"--robot-type:             {args.robot_type}\n"
+      f"--robot-name:             {args.robot_name}\n"
+      f"--robot-command-topic:    {args.robot_command_topic} \n"
+      f"--robot-initial-position: {args.robot_initial_position} \n")
 
   if 'bluerov' in args.robot_type:
     rospy.loginfo( f"simulating water surface depth at {args.water_surface_depth}" )
 
-  fake_robots = []
+  fake_robots = [ ]
 
-  for robot_type, robot_name, robot_command_topic in zip( args.robot_type, args.robot_name, args.robot_command_topic ):
-    fake_robots += [Simulation(robot_type, robot_name, robot_command_topic, args.water_surface_depth)]
+  for robot_type, robot_name, robot_command_topic, robot_initial_position in zip(
+      args.robot_type, args.robot_name, args.robot_command_topic, args.robot_initial_position
+      ):
+    fake_robots += [ Simulation(
+        robot_type, robot_name, robot_command_topic, args.water_surface_depth, robot_initial_position
+        ) ]
 
   rate = rospy.Rate( 100. )
 
@@ -130,7 +160,7 @@ def publish_fake_packets( args: Namespace ):
       robot.model.step()
       robot.pose.pose.pose = robot.interface.ros_pose_from_state( robot.model.state )
       robot.pose.header.seq += 1
-      robot.pose.pose.covariance = (0.01 * eye(6, dtype=float)).flatten().tolist()
+      robot.pose.pose.covariance = (0.01 * eye( 6, dtype = float )).flatten().tolist()
       robot.pose.header.stamp = rospy.Time.now()
       robot.publisher.publish( robot.pose )
 
